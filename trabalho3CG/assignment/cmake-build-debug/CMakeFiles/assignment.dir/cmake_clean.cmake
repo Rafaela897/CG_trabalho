27b7a4d5bc49c5c53a6697c13c9eccb871990e00
Rafaela897 <rpinho897@gmail.com>
2018-05-12 20:40:55 +0100

    .

---
+++ trabalho3CG/assignment/cmake-build-debug/CMakeFiles/assignment.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/assignment.dir/engine.cpp.o"
+  "CMakeFiles/assignment.dir/generator.cpp.o"
+  "CMakeFiles/assignment.dir/tinyxml2.cpp.o"
+  "assignment.pdb"
+  "assignment"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/assignment.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ trabalho3CG/assignment/cmake-build-default/CMakeFiles/assignment.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/rafaela/Desktop/CG_trabalho/trabalho3CG/assignment/engine.cpp" "/home/rafaela/Desktop/CG_trabalho/trabalho3CG/assignment/cmake-build-default/CMakeFiles/assignment.dir/engine.cpp.o"
+  "/home/rafaela/Desktop/CG_trabalho/trabalho3CG/assignment/generator.cpp" "/home/rafaela/Desktop/CG_trabalho/trabalho3CG/assignment/cmake-build-default/CMakeFiles/assignment.dir/generator.cpp.o"
+  "/home/rafaela/Desktop/CG_trabalho/trabalho3CG/assignment/tinyxml2.cpp" "/home/rafaela/Desktop/CG_trabalho/trabalho3CG/assignment/cmake-build-default/CMakeFiles/assignment.dir/tinyxml2.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
